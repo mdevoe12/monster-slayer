208d232f345fcfe7a1709dd75c7d69bfdfe231f3
Matt DeVoe <matthew.devoe@gmail.com>
2018-04-27 11:01:55 -0600

    win/loss conditions and restart logic added

--- app.js
+++ app.js
@@ -12,15 +12,13 @@ new Vue({
       this.monsterHealth = 100
     },
     attack: function() {
-      var max = 10
-      var min = 3
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
-      this.monsterHealth -= damage
+      this.monsterHealth -= this.calculateDamage(3, 10)
+      if (this.checkWin()) {
+        return
+      }
       
-      max = 12
-      min = 5
-      damage = Math.max(Math.floor(Math.random() * max) + 1, min)
-      this.playerHealth -= damage
+      this.playerHealth -= this.calculateDamage(5, 12)
+      this.checkWin()
     },
     specialAttack: function () {
       
@@ -30,6 +28,27 @@ new Vue({
     },
     giveUp: function() {
       
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You Won! New Game?')) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false
+        }
+        return true
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You Lost! New Game?')) {
+          this.startGame()
+        } else {
+          this.gameIsRunning = false
+        }
+        return true
+      }
+      return false
     }
   }
 })
